d660dd672e86c96e35a88c958c635172ea8d483a
Trollbert <poparobert2012@gmail.com>
2026-05-24 16:50:15 +0300

    nu stiu php

--- php_dir/app/views/dashboard_view.php
+++ php_dir/app/views/dashboard_view.php
@@ -2,6 +2,12 @@
 <div class="container">
     <h2>Dashboard</h2>
     <p>Welcome, <?php echo htmlspecialchars($currentUser ?? ''); ?>!</p>
+    <p>Tables you have access to:</p>
+    <ul>
+        <?php foreach ($inventoryTableIDs as $id): ?>
+            <li><?php echo $id; ?></li>
+        <?php endforeach; ?>
+    </ul>
     <p><a href="index.php?action=logout">Logout</a></p>
 </div>
 <?php require_once __DIR__ . '/../../footer.php'; ?>
