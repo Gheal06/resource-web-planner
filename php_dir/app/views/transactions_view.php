b6fda0413728287040e8d3a56428fc61f88c10e9
Gheal06 <alexandru.gheorghies@gmail.com>
2026-05-26 12:59:23 +0300

    inventory_view

---
+++ php_dir/app/views/transactions_view.php
@@ -0,0 +1,40 @@
+<div class="split-container3-m center-conent">
+<h2 class="center-content">Recurring Transactions</h2>
+<table id="transaction-table">
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Frequency</th>
+            <th>Time</th>
+        </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>instalator</td>
+        <td>Vine instalatorul sa repare ac-ul</td>
+        <td>One Time</td>
+        <td>Tue 26 May 2026, 14:00</td>
+    </tr>
+    <tr>
+        <td>Lemn</td>
+        <td>Gigel vine cu lemnul</td>
+        <td>Weekly</td>
+        <td>Wed, 14:00</td>
+    </tr>
+    <tr>
+        <td>salarii</td>
+        <td>Salarii</td>
+        <td>Monthly</td>
+        <td>01, 00:00</td>
+    </tr>
+    <tr>
+        <td><a href="index.php?action=create-transaction">Create new transaction</a></td>
+        <!-- de pus id-ul inventory-ului in linkul de mai sus -->
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    </tbody>
+</table>
+</div>
