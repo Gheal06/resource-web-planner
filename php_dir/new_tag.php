ca085714d30a1307c4193bb6b45784876b323238
Gheal06 <alexandru.gheorghies@gmail.com>
2026-05-27 18:47:53 +0300

    add tag view

---
+++ php_dir/new_tag.php
@@ -0,0 +1,11 @@
+<?php
+require_once "header.php";
+//require_once "app/controllers/ResourceController.php";
+
+$createTagAction = 'new_tag.php';
+// $resourceController = new ResourceController();
+// $message = $resourceController -> addResource(); 
+require_once "app/views/header_view.php";
+require_once "app/views/new_tag_view.php";
+require_once "footer.php";
+?>
