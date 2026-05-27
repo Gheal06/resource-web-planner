8974d9e4360856be3ed39b3de9fd1cf34de6d243
Trollbert <poparobert2012@gmail.com>
2026-05-27 11:45:40 +0300

    cel mai bun limbaj

--- php_dir/app/models/FonduriModel.php
+++ php_dir/app/models/FonduriModel.php
@@ -8,24 +8,24 @@ class FonduriModel {
     }
 
     public function getFonduriByInventoryId($inventory_id) {
-        $res = pg_query_params($this->connection, "SELECT * FROM fonduri WHERE inventory_id = $1", array($inventory_id));
+        $res = @pg_query_params($this->connection, "SELECT * FROM fonduri WHERE inventory_id = $1", array($inventory_id));
         if (!$res) return null;
         $fonduri = array();
-        while ($row = pg_fetch_assoc($res)) {
+        while ($row = @pg_fetch_assoc($res)) {
             $fonduri[] = $row;
         }
         return $fonduri;
     }
 
     public function getFonduriByInventoryIdAndCurrency($inventory_id, $currency_code) {
-        $res = pg_query_params($this->connection, "SELECT * FROM fonduri WHERE inventory_id = $1 AND currency_code = $2", array($inventory_id, $currency_code));
+        $res = @pg_query_params($this->connection, "SELECT * FROM fonduri WHERE inventory_id = $1 AND currency_code = $2", array($inventory_id, $currency_code));
         if (!$res) return null;
-        return pg_fetch_assoc($res);
+        return @pg_fetch_assoc($res);
     }
 
     public function addFonduri($inventory_id, $amount, $currency_code) {
         // Try to update existing row by increasing the amount
-        $res = pg_query_params($this->connection,
+        $res = @pg_query_params($this->connection,
             "UPDATE fonduri SET amount = amount + $1 WHERE inventory_id = $2 AND currency_code = $3",
             array($amount, $inventory_id, $currency_code)
         );
@@ -35,24 +35,22 @@ class FonduriModel {
         }
 
         // If no row was updated, insert a new one (amount is the initial value)
-        return pg_query_params($this->connection,
+        return @pg_query_params($this->connection,
             "INSERT INTO fonduri (amount, currency_code, inventory_id) VALUES ($1, $2, $3)",
             array($amount, $currency_code, $inventory_id)
         );
     }
 
     public function setFonduri($inventory_id, $amount, $currency_code) {
-        $res = pg_query_params($this->connection, "SELECT id FROM fonduri WHERE inventory_id = $1 AND currency_code = $2", array($inventory_id, $currency_code));
+        $res = @pg_query_params($this->connection, "SELECT id FROM fonduri WHERE inventory_id = $1 AND currency_code = $2", array($inventory_id, $currency_code));
         if (!$res) return null;
-        $row = pg_fetch_assoc($res);
+        $row = @pg_fetch_assoc($res);
         if ($row) {
-            return pg_query_params($this->connection, "UPDATE fonduri SET amount = $1 WHERE id = $2", array($amount, $row['id']));
+            return @pg_query_params($this->connection, "UPDATE fonduri SET amount = $1 WHERE id = $2", array($amount, $row['id']));
         } else {
-            return pg_query_params($this->connection, "INSERT INTO fonduri (amount, currency_code, inventory_id) VALUES ($1, $2, $3)", array($amount, $currency_code, $inventory_id));
+            return @pg_query_params($this->connection, "INSERT INTO fonduri (amount, currency_code, inventory_id) VALUES ($1, $2, $3)", array($amount, $currency_code, $inventory_id));
         }
     }
 
 }
-
 ?>
-
