42ef67391fbbee53af810e0f03760266cd9e29d3
Trollbert <poparobert2012@gmail.com>
2026-05-27 09:47:29 +0300

    merged FonduriService into InvService

--- php_dir/app/services/InventoryManagementService.php
+++ php_dir/app/services/InventoryManagementService.php
@@ -2,11 +2,13 @@
 require_once __DIR__ . "/../models/InventoryModel.php";
 require_once __DIR__ . "/../models/InventoryPermissionsModel.php";
 require_once __DIR__ . "/../models/UserModel.php";
+require_once __DIR__ . "/../models/FonduriModel.php";
 
   class InventoryManagementService {
     private $inventoryModel;
     private $inventoryPermissionsModel;
     private $userModel;
+    private $fonduriModel;
 
     private $readPermissionMask = 1;
     private $insertPermissionMask = 2;
@@ -17,6 +19,7 @@ require_once __DIR__ . "/../models/UserModel.php";
       $this->inventoryModel= new InventoryModel($connection);
       $this->inventoryPermissionsModel = new InventoryPermissionsModel($connection);
       $this->userModel = new UserModel($connection);
+      $this->fonduriModel = new FonduriModel($connection);
     }
     public function getUserInventoryIDsByMask($username, $permission_mask) {
         return $this->inventoryPermissionsModel->getUserInventoryIDsByMask($username, $permission_mask);
@@ -80,6 +83,32 @@ require_once __DIR__ . "/../models/UserModel.php";
     public function updateInventoryName($id, $name) {
 
       return $this->inventoryModel->update($id, $name, null);
+    }
+
+    public function getFonduriByInventoryId($inventory_id) {
+      $res = $this->fonduriModel->getFonduriByInventoryId($inventory_id);
+      return $res === null ? array() : $res;
+    }
+
+    public function getFonduriByInventoryIdAndCurrency($inventory_id, $currency_code) {
+      $res = $this->fonduriModel->getFonduriByInventoryIdAndCurrency($inventory_id, $currency_code);
+      return $res === null ? null : $res;
+    }
+
+    public function addFonduri($inventory_id, $amount, $currency_code) {
+      $res = $this->fonduriModel->addFonduri($inventory_id, $amount, $currency_code);
+      if ($res === false) {
+        return array('success' => false, 'message' => 'Failed to add funds.');
+      }
+      return array('success' => true, 'message' => 'Funds added.');
+    }
+
+    public function setFonduri($inventory_id, $amount, $currency_code) {
+      $res = $this->fonduriModel->setFonduri($inventory_id, $amount, $currency_code);
+      if ($res === false) {
+        return array('success' => false, 'message' => 'Failed to set funds.');
+      }
+      return array('success' => true, 'message' => 'Funds set.');
     }
   }
 ?>
