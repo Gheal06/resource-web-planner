9156c821cb17828579e61cd1054746eb22e5bdaa
Trollbert <poparobert2012@gmail.com>
2026-05-27 16:46:48 +0300

    tags

--- php_dir/app/models/ResurseModel.php
+++ php_dir/app/models/ResurseModel.php
@@ -68,6 +68,63 @@ class ResurseModel {
         return array('success' => true, 'message' => 'Amount set.');
     }
 
+    /**
+     * Get all tags for a resource
+     */
+    public function getTagsForResource($resource_id) {
+        $sql = "SELECT t.* FROM tags t JOIN has_tag ht ON ht.tag_id = t.id WHERE ht.resource_id = $1";
+        $res = @pg_query_params($this->connection, $sql, array($resource_id));
+        if (!$res) return array();
+        $rows = array();
+        while ($row = pg_fetch_assoc($res)) {
+            $rows[] = $row;
+        }
+        return $rows;
+    }
+
+    /**
+     * Get resources by a list of tag IDs (intersection - resource must have ALL tags)
+     * $tag_ids is an array of tag IDs
+     */
+    public function getResourcesByTags($inventory_id, $tag_ids) {
+        if (empty($tag_ids)) return array();
+        
+        // Build placeholders for tag_ids: $1, $2, ...
+        $placeholders = array();
+        for ($i = 0; $i < count($tag_ids); $i++) {
+            $placeholders[] = '$' . ($i + 2);
+        }
+        $placeholder_str = implode(',', $placeholders);
+        
+        // COUNT ensures resource has ALL tags (intersection)
+        $sql = "SELECT DISTINCT r.* FROM resources r 
+                JOIN has_tag ht ON ht.resource_id = r.id 
+                WHERE r.inventory_id = $1 AND ht.tag_id IN ($placeholder_str) 
+                GROUP BY r.id 
+                HAVING COUNT(DISTINCT ht.tag_id) = " . count($tag_ids);
+        
+        $params = array_merge(array($inventory_id), $tag_ids);
+        $res = @pg_query_params($this->connection, $sql, $params);
+        if (!$res) return array();
+        $rows = array();
+        while ($row = pg_fetch_assoc($res)) {
+            $rows[] = $row;
+        }
+        return $rows;
+    }
+
+    /**
+     * Get all tags in the system
+     */
+    public function getAllTags() {
+        $res = @pg_query_params($this->connection, "SELECT * FROM tags ORDER BY name", array());
+        if (!$res) return array();
+        $rows = array();
+        while ($row = pg_fetch_assoc($res)) {
+            $rows[] = $row;
+        }
+        return $rows;
+    }
 }
 
 ?>
