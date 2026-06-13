076e9479dd58c1b364460a56de0bc17875f91fae
Trollbert <poparobert2012@gmail.com>
2026-06-13 10:44:08 +0300

    ce face gitul

--- php_dir/report.php
+++ php_dir/report.php
@@ -32,7 +32,7 @@
         $endDate = str_to_ts($_POST['end_date'], 'infinity');
         if($endDate!='infinity')
             $endDate=date('Y-m-d', strtotime($endDate.' +1 day'));
-        if(!isset($_POST['submit_generate_html']) || !isset($inventoryId) || !isset($resourceId))
+        if(!isset($inventoryId) || !isset($resourceId))
             error();
         if(strlen($resourceId)==0) error();
         $tp = $resourceId[0];
