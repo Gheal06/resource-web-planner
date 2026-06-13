dc6d9862af5162f1fe708554d1b13dd050be2044
Trollbert <poparobert2012@gmail.com>
2026-06-13 10:47:49 +0300

    htmlspeciahchars

--- php_dir/app/views/report_view.php
+++ php_dir/app/views/report_view.php
@@ -6,18 +6,18 @@
     $endDate = ($endDate == 'infinity' ? '+∞' : date('d-m-y h:i A', strtotime($endDate.'-1 second')));
     $thresholdAmount = $resource['threshold_amount'] ?? 0;
 ?>
-<h2>Report (<?php echo $startDate.' -> '.$endDate; ?>)</h2>
-<h3><?php echo $inventory['name'];?></h3>
+<h2>Report (<?php echo htmlspecialchars($startDate.' -> '.$endDate); ?>)</h2>
+<h3><?php echo htmlspecialchars($inventory['name']);?></h3>
 <h4>
 <?php 
 $qtyChangeStr;
 if($tp == 'f'){
     $qtyChangeStr = 'amount_change';
-    echo "Fund: " . $resource['currency_code'] . (strlen($resource['name'] ?? '')>0 ? ' (' . $resource['name'] . ')': '');
+    echo htmlspecialchars("Fund: " . $resource['currency_code'] . (strlen($resource['name'] ?? '')>0 ? ' (' . $resource['name'] . ')': ''));
 }
 else{
     $qtyChangeStr = 'quantity_change';
-    echo "Resource: " . $resource['name'];
+    echo htmlspecialchars("Resource: " . $resource['name']);
 }
 $endingBalance = floatval($startingBalance);
 $minBalance = $startingBalance;
@@ -53,24 +53,24 @@ $unit = $tp == 'f' ? ($resource['currency_code'] ?? '') : ($resource['unit'] ?? 
         $maxBalance = max($endingBalance, $maxBalance);
         // echo date('D, d M Y H:i', strtotime($entry['created_at'])) . ' -> ' . $delta;
     ?>
-    <td> <?php echo $i++; ?> </td>
-    <td> <?php echo date('D, d M Y H:i', strtotime($entry['created_at'])); ?> </td>
-    <td> <?php echo $delta.' '.$unit; ?> </td>
-    <td> <?php echo $endingBalance-$delta.' '.$unit; ?> </td>
-    <td> <?php echo $endingBalance.' '.$unit; ?> </td>
+    <td> <?php echo htmlspecialchars($i++); ?> </td>
+    <td> <?php echo htmlspecialchars(date('D, d M Y H:i', strtotime($entry['created_at']))); ?> </td>
+    <td> <?php echo htmlspecialchars($delta.' '.$unit); ?> </td>
+    <td> <?php echo htmlspecialchars($endingBalance-$delta.' '.$unit); ?> </td>
+    <td> <?php echo htmlspecialchars($endingBalance.' '.$unit); ?> </td>
 </tr>
 <?php endforeach; ?> 
 </tbody>
 </table>
 
 <ul>
-<li>Total Transactions: <?php echo count($history); ?></li>
-<li>Starting Balance: <?php echo $startingBalance.' '.$unit; ?></li>
-<li>Ending Balance: <?php echo $endingBalance.' '.$unit; ?></li>
-<li>Overall Change: <?php echo $endingBalance - $startingBalance.' '.$unit; ?></li>
-<li>Min Balance: <?php echo $minBalance.' '.$unit; ?></li>
-<li>Max Balance: <?php echo $maxBalance.' '.$unit; ?></li>
-<li>Critical Balance: <?php echo $thresholdAmount.' '.$unit; ?></li>
+<li>Total Transactions: <?php echo htmlspecialchars(count($history)); ?></li>
+<li>Starting Balance: <?php echo htmlspecialchars($startingBalance.' '.$unit); ?></li>
+<li>Ending Balance: <?php echo htmlspecialchars($endingBalance.' '.$unit); ?></li>
+<li>Overall Change: <?php echo htmlspecialchars($endingBalance - $startingBalance.' '.$unit); ?></li>
+<li>Min Balance: <?php echo htmlspecialchars($minBalance.' '.$unit); ?></li>
+<li>Max Balance: <?php echo htmlspecialchars($maxBalance.' '.$unit); ?></li>
+<li>Critical Balance: <?php echo htmlspecialchars($thresholdAmount.' '.$unit); ?></li>
 </ul>
 
 
